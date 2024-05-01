341aacb043c9195e57c75071578ce0c88ce903cd
sgrumley-anz <137256426+sgrumley-anz@users.noreply.github.com>
2024-05-01 18:23:16 +1000

    modified

--- init.lua
+++ init.lua
@@ -1,23 +1,2 @@
---[[
-  File: init.lua
-  Description: Entry point file for neovim
-]]
-
--- Bootsraping plugin manager
-require("lazy-bootstrap")
-
--- Settings
-require("config.keymaps")
-require("config.options")
-
--- Plugin management
-require("lazy").setup({
-	{ import = "base.dashboard" },
-	{ import = "base.search" },
-	{ import = "base.lsp" },
-	{ import = "base.test" },
-	{ import = "base.lang" },
-	{ import = "plugins" },
-}, {})
-
--- require("config.go-test")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/plugins/config/goto.lua
+++ lua/plugins/config/goto.lua
@@ -1,13 +1,13 @@
 local gt = require("goto-preview")
 gt.setup({
-	height = 25,
-	width = 150,
+  height = 25,
+  width = 150,
 })
 
 -- keymaps
-nmap("gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "[P]eek Definition")
-nmap("gpt", "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "[P]eek Type Definition")
-nmap("gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", "[P]eek Implementation")
-nmap("gpD", "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>", "[P]eek Type Declaration")
-nmap("gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>", "[P]eek Type Declaration")
-nmap("gP", "<cmd>lua require('goto-preview').goto_preview_close_all_win()<CR>", "[P]eek Type Declaration")
+vim.keymap.set("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+vim.keymap.set("n", "gpt", "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>")
+vim.keymap.set("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+vim.keymap.set("n", "gpD", "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>")
+vim.keymap.set("n", "gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>")
+vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').goto_preview_close_all_win()<CR>")
