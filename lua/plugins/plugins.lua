ea697ead3e59c5ee039ea19ea1db337094dda051
sgrumley-anz <137256426+sgrumley-anz@users.noreply.github.com>
2024-06-07 15:05:49 +1000

    fixed todo scheme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -263,26 +263,25 @@ return {
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
-		keys = {
-			{
-				"]t",
-				function()
-					require("todo-comments").jump_next()
-				end,
-				desc = "Next todo comment",
-			},
-			{
-				"[t",
-				function()
-					require("todo-comments").jump_prev()
-				end,
-				desc = "Previous todo comment",
-			},
-			{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-			{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-			{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
-		},
+		-- for some reason the below keys stops highlighting??
+		-- keys = {
+		-- 	{
+		-- 		"]t",
+		-- 		function()
+		-- 			require("todo-comments").jump_next()
+		-- 		end,
+		-- 		desc = "Next todo comment",
+		-- 	},
+		-- 	{
+		-- 		"[t",
+		-- 		function()
+		-- 			require("todo-comments").jump_prev()
+		-- 		end,
+		-- 		desc = "Previous todo comment",
+		-- 	},
+		-- 	{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+		-- 	{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+		-- },
 	},
 
 	-- Status Line
