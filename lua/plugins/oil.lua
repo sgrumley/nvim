4eae7cff89856b4e2e2e5e8d4120660ccee48a41
sgrumley <grumsamu@gmail.com>
2024-08-04 03:20:55 +1000

    refine oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,10 +4,25 @@ return {
 		opts = {},
 		config = function()
 			local oil = require("oil")
-			oil.setup()
-			vim.keymap.set("n", "-", oil.toggle_float, {})
+			oil.setup({
+				float = {
+					max_width = 110,
+					max_height = 25,
+				},
+				keymaps = {
+					["q"] = "actions.close",
+				},
+			})
+			vim.keymap.set("n", "-", oil.toggle_float, { desc = "Oil" })
 		end,
-		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- - up a dir
+		-- _ current working dir
+		-- g. toggle hidden
+		-- g\\ toggle trash
+		-- gs change sort
+		-- <C-p> preview
+		-- <C-c> close
+		-- <C-l> refresh
 	},
 }
