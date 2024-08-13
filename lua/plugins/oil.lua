1faac4c3bacb09589d2bf86c1262093038998ded
sgrumley <grumsamu@gmail.com>
2024-08-13 23:54:28 +1000

    added git status to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,6 +12,10 @@ return {
 				keymaps = {
 					["q"] = "actions.close",
 				},
+				-- this is for the oil-vcs-status plugin
+				win_options = {
+					signcolumn = "number",
+				},
 			})
 			vim.keymap.set("n", "-", oil.toggle_float, { desc = "Oil" })
 		end,
@@ -24,5 +28,72 @@ return {
 		-- <C-p> preview
 		-- <C-c> close
 		-- <C-l> refresh
+	},
+	{
+		"SirZenith/oil-vcs-status",
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+		config = function()
+			local status_const = require("oil-vcs-status.constant.status")
+			local StatusType = status_const.StatusType
+
+			vim.api.nvim_set_hl(0, "OilVcsStatusAdded", { fg = "#a6e3a1" })
+			vim.api.nvim_set_hl(0, "OilVcsStatusModified", { fg = "#fab387" })
+			vim.api.nvim_set_hl(0, "OilVcsStatusDeleted", { fg = "#f38ba8" })
+
+			require("oil-vcs-status").setup({
+				status_symbol = {
+					[StatusType.Added] = "",
+					[StatusType.Copied] = "󰆏",
+					[StatusType.Deleted] = "",
+					[StatusType.Ignored] = "",
+					[StatusType.Modified] = "",
+					[StatusType.Renamed] = "",
+					[StatusType.TypeChanged] = "󰉺",
+					[StatusType.Unmodified] = " ",
+					[StatusType.Unmerged] = "",
+					[StatusType.Untracked] = "",
+					[StatusType.External] = "",
+
+					[StatusType.UpstreamAdded] = "󰈞",
+					[StatusType.UpstreamCopied] = "󰈢",
+					[StatusType.UpstreamDeleted] = "",
+					[StatusType.UpstreamIgnored] = " ",
+					[StatusType.UpstreamModified] = "󰏫",
+					[StatusType.UpstreamRenamed] = "",
+					[StatusType.UpstreamTypeChanged] = "󱧶",
+					[StatusType.UpstreamUnmodified] = " ",
+					[StatusType.UpstreamUnmerged] = "",
+					[StatusType.UpstreamUntracked] = " ",
+					[StatusType.UpstreamExternal] = "",
+				},
+				status_hl_group = {
+					[StatusType.Added] = "OilVcsStatusAdded",
+					[StatusType.Copied] = "OilVcsStatusCopied",
+					[StatusType.Deleted] = "OilVcsStatusDeleted",
+					[StatusType.Ignored] = "OilVcsStatusIgnored",
+					[StatusType.Modified] = "OilVcsStatusModified",
+					[StatusType.Renamed] = "OilVcsStatusRenamed",
+					[StatusType.TypeChanged] = "OilVcsStatusTypeChanged",
+					[StatusType.Unmodified] = "OilVcsStatusUnmodified",
+					[StatusType.Unmerged] = "OilVcsStatusUnmerged",
+					[StatusType.Untracked] = "OilVcsStatusUntracked",
+					[StatusType.External] = "OilVcsStatusExternal",
+
+					[StatusType.UpstreamAdded] = "OilVcsStatusUpstreamAdded",
+					[StatusType.UpstreamCopied] = "OilVcsStatusUpstreamCopied",
+					[StatusType.UpstreamDeleted] = "OilVcsStatusUpstreamDeleted",
+					[StatusType.UpstreamIgnored] = "OilVcsStatusUpstreamIgnored",
+					[StatusType.UpstreamModified] = "OilVcsStatusUpstreamModified",
+					[StatusType.UpstreamRenamed] = "OilVcsStatusUpstreamRenamed",
+					[StatusType.UpstreamTypeChanged] = "OilVcsStatusUpstreamTypeChanged",
+					[StatusType.UpstreamUnmodified] = "OilVcsStatusUpstreamUnmodified",
+					[StatusType.UpstreamUnmerged] = "OilVcsStatusUpstreamUnmerged",
+					[StatusType.UpstreamUntracked] = "OilVcsStatusUpstreamUntracked",
+					[StatusType.UpstreamExternal] = "OilVcsStatusUpstreamExternal",
+				},
+			})
+		end,
 	},
 }
