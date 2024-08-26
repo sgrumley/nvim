66fe08f0e7b4a530e5257f0302d9b98714c0fdb2
sgrumley-anz <137256426+sgrumley-anz@users.noreply.github.com>
2024-08-26 21:31:48 +1000

    updated linting and formatting

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -17,34 +17,33 @@ return {
 			notify_on_error = false,
 			format_on_save = function(bufnr)
 				local disable_filetypes = {
-					dockerfile = true,
+					-- dockerfile = true,
+					-- sql = true,
 					yaml = true,
 					json = true,
 					proto = true,
-					sql = true,
 					md = true,
 				}
 				return {
-					timeout_ms = 500,
+					timeout_ms = 200,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 				}
 			end,
 			formatters_by_ft = {
 				go = { "gofumpt", "goimports-reviser" }, -- golines
-				-- rust = { "rustfmt" },
 				sql = { "sql_formatter" }, -- sqlfluff
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" }, -- yamlfix, yamlfmt
+				javascript = { "biome" },
+				typescript = { "biome" },
+				javascriptreact = { "biome" },
+				typescriptreact = { "biome" },
+				svelte = { "biome" },
+				css = { "biome" },
+				html = { "biome" },
+				json = { "biome" },
+				yaml = { "yamlfmt" }, -- yamlfix, prettier
 				markdown = { "prettier" },
 				lua = { "stylua" },
-				-- python = { "isort", "black" },
+				terraform = { "terraform_fmt" },
 				proto = { "buf" },
 			},
 		},
