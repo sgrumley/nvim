c0487ab0d82904a74062ec5749d6d55a2db4947a
sgrumley <grumsamu@gmail.com>
2024-12-01 22:01:58 +1100

    updated nvui to v3

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,89 +1,35 @@
-local options = {
+---@type ChadrcConfig
+local M = {}
 
-	base46 = {
-		theme = "catppuccin", -- default theme
-		hl_add = {},
-		hl_override = {},
-		integrations = {},
-		changed_themes = {},
-		transparency = false,
-		theme_toggle = { "onedark", "one_light" },
-	},
-
-	ui = {
-		cmp = {
-			icons = true,
-			lspkind_text = true,
-			style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-		},
-
-		telescope = { style = "bordered" }, -- borderless / bordered
-
-		statusline = {
-			theme = "default", -- default/vscode/vscode_colored/minimal
-			-- default/round/block/arrow separators work only for default statusline theme
-			-- round and block will work for minimal theme only
-			separator_style = "round",
-			order = nil,
-			modules = nil,
-		},
-
-		-- lazyload it when there are 1+ buffers
-		tabufline = {
-			enabled = false,
-			lazyload = true,
-			order = { "treeOffset", "buffers", "tabs", "btns" },
-			modules = nil,
-		},
-
-		nvdash = {
-			load_on_startup = false,
-
-			header = {
-				"           ▄ ▄                   ",
-				"       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-				"       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-				"    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-				"  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-				"  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-				"▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-				"█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-				"    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-			},
-
-			buttons = {
-				{ "  Find File", "Spc f f", "Telescope find_files" },
-				{ "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-				{ "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-				{ "  Bookmarks", "Spc m a", "Telescope marks" },
-				{ "  Themes", "Spc t h", "Telescope themes" },
-				{ "  Mappings", "Spc c h", "NvCheatsheet" },
-			},
-		},
-	},
-
-	term = {
-		winopts = { number = false, relativenumber = false },
-		sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-		float = {
-			relative = "editor",
-			row = 0.3,
-			col = 0.25,
-			width = 0.5,
-			height = 0.4,
-			border = "single",
-		},
-	},
-
-	lsp = { signature = false },
-
-	cheatsheet = {
-		theme = "grid", -- simple/grid
-		excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
-	},
-
-	mason = { cmd = true, pkgs = {} },
+M.base46 = {
+	theme = "catppuccin",
 }
 
-local status, chadrc = pcall(require, "chadrc")
-return vim.tbl_deep_extend("force", options, status and chadrc or {})
+M.ui = {
+	statusline = {
+		enabled = true,
+		theme = "default",
+		separator_style = "round",
+	},
+	telescope = {
+		style = "bordered",
+	},
+	lsp = {
+		signature = true,
+	},
+	mason = {
+		pkgs = {},
+		skip = {},
+	},
+	colify = {
+		enabled = true,
+		mode = "virtual",
+		virt_text = "󱓻 ",
+		highlight = {
+			hex = true,
+			lspvars = true,
+		},
+	},
+}
+
+return M
