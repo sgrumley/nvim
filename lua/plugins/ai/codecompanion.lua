01c20570e5116ad29cd5920fc80c4797f0b63575
sgrumley <grumsamu@gmail.com>
2025-07-03 23:28:44 +1000

    added codecompanion

--- lua/plugins/ai/codecompanion.lua
+++ lua/plugins/ai/codecompanion.lua
@@ -1,13 +1,15 @@
 -- consider extending with MCP Hub https://github.com/ravitemer/mcphub.nvim
+-- on setup use :Copilot setup to configure auth
+-- https://codecompanion.olimorris.dev/usage/inline-assistant.html#classification
+-- TODO: look at making my own prompt templates for things like tests
 return {
-	-- required neovim 11 + newer node version
-	-- {
-	-- 	"olimorris/codecompanion.nvim",
-	-- 	opts = {},
-	-- 	dependencies = {
-	-- 		"github/copilot.vim",
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 	},
-	-- },
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {},
+		dependencies = {
+			"github/copilot.vim",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
 }
